6949d9ea7fa6487de3aef8ab03fa819eafc834f9
angel <angelgarces1248@gmail.com>
2023-05-14 21:03:04 -0400

    [feat] health check

--- src/main/java/com/example/springboot/controller/GlobalController.java
+++ src/main/java/com/example/springboot/controller/GlobalController.java
@@ -4,28 +4,35 @@ import com.example.springboot.dto.HomeDTO;
 import com.example.springboot.service.CategoryService;
 import com.example.springboot.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/home")
-public class HomeController {
+@RequestMapping()
+public class GlobalController {
     @Autowired
     ProductService productService;
     @Autowired
     CategoryService categoryService;
 
+    @GetMapping("/health")
+    public ResponseEntity<Object> healthCheck() {
+        // add custom logic to check the health of your application
+        return new ResponseEntity<>("Application is running", HttpStatus.OK);
+    }
+
     // ===============
     //    get EPs   //
     // ===============
 
-    @GetMapping
+    @GetMapping("api/home")
     @ResponseBody
     public HomeDTO getHomePage(){
         HomeDTO body = new HomeDTO();
